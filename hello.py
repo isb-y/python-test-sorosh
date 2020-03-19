6abb8eb39f4f51739b0095e7f392957ba9473ebd
isby <ibrahim.yigci@gmail.com>
2020-03-19 11:44:43 +0100

    added something new

--- hello.py
+++ hello.py
@@ -1,13 +1,5 @@
-#Commit Test
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.random.randn(2, 2)
-print(x)
-
-# Let's change stuff
-y = 3024983
-z = y * x
-
-# 11.03.2020
+def addTwoNumbers(a, b):
+    return a + b
